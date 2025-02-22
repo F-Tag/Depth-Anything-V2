f2b5719df2710e6418a17df3f7eb5a4bedbc3f10
Fumiaki Taguchi <25476669+F-Tag@users.noreply.github.com>
2025-02-22 16:16:57 +0900

    update

--- metric_depth/commands.ps1
+++ metric_depth/commands.ps1
@@ -1,3 +1,3 @@
 python depth_to_pointcloud.py --encoder vits --load-from models/depth_anything_v2_metric_vkitti_vits.pth --max-depth 80 --img-path vlcsnap-2025-02-21-22h04m55s001.png --outdir .\outputs
-python .\run.py --encoder vits --load-from models/depth_anything_v2_metric_vkitti_vits.pth --max-depth 80 --img-path vlcsnap-2025-02-21-22h04m55s001.png --outdir .\outputs --grayscale
+python run.py --encoder vits --load-from models/depth_anything_v2_metric_vkitti_vits.pth --max-depth 80 --img-path vlcsnap-2025-02-21-22h04m55s001.png --outdir outputs --grayscale
 python .\show_pointcloud.py  
--- metric_depth/export_onnx.py
+++ metric_depth/export_onnx.py
@@ -1,14 +1,28 @@
-import torch
-from depth_anything_v2.dpt import DepthAnythingV2
+import os
+
 import numpy as np
 import onnx
+import torch
 from onnxconverter_common import float16
+from onnxsim import simplify
+
+from depth_anything_v2.dpt import DepthAnythingV2
+
 
 def main():
     # load models
-    max_depth = 80
-    depth_anything = DepthAnythingV2(**{**{'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]}, 'max_depth': max_depth})
-    depth_anything.load_state_dict(torch.load("models/depth_anything_v2_metric_vkitti_vits.pth", map_location='cpu'))
+    max_depth = 80  # for outdoor
+    depth_anything = DepthAnythingV2(
+        **{
+            **{"encoder": "vits", "features": 64, "out_channels": [48, 96, 192, 384]},
+            "max_depth": max_depth,
+        }
+    )
+    depth_anything.load_state_dict(
+        torch.load(
+            "models/depth_anything_v2_metric_vkitti_vits.pth", map_location="cpu"
+        )
+    )
     depth_anything = depth_anything.to("cpu").eval()
 
     # opencv style bgr numpy array (full hd)
@@ -16,25 +30,38 @@ def main():
     input_tensor, (h, w) = depth_anything.image2tensor(input_image, 518)
     input_tensor = input_tensor.to("cpu")
 
+    os.makedirs("export_models", exist_ok=True)
+
     # export to onnx
     torch.onnx.export(
         depth_anything,
         input_tensor,
-        "models/depth_anything_v2_metric_vkitti_vits.onnx",
+        "export_models/depth_anything_v2_metric_vkitti_vits.org.onnx",
         export_params=True,
         opset_version=11,
         do_constant_folding=True,
         input_names=["input_image"],
         output_names=["output_depth"],
-        dynamic_axes={"input_image": {0: "batch_size"}, "output_depth": {0: "batch_size"}},
+        dynamic_axes={
+            "input_image": {0: "batch_size"},
+            "output_depth": {0: "batch_size"},
+        },
     )
 
     # postprocess model
-    onnx_model = onnx.load("models/depth_anything_v2_metric_vkitti_vits.onnx")
+    onnx_model = onnx.load(
+        "export_models/depth_anything_v2_metric_vkitti_vits.org.onnx"
+    )
     onnx_model = onnx.shape_inference.infer_shapes(onnx_model)
+
+    onnx_model, check = simplify(onnx_model)
+    assert check
+
     onnx_model = float16.convert_float_to_float16(onnx_model, keep_io_types=True)
-    onnx.save(onnx_model, "models/depth_anything_v2_metric_vkitti_vits_shape.onnx")
+    onnx.save(
+        onnx_model, "export_models/depth_anything_v2_metric_vkitti_vits_shape.onnx"
+    )
 
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     main()
